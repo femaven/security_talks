c3ca5e501df3fb84548f1f638281b5543f6e36bb
femaven <ariswadkar@gmail.com>
2017-03-31 15:55:51 -0500

    Cyphercon 2.0 2017 talk
    I thought DLP was supposed to fix this
    Includes Deck and Mindmap (use Freemind)

---
+++ DLP Analytics- Cyphercon.mm
@@ -0,0 +1,104 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1487611097126" ID="ID_1464509632" MODIFIED="1487612339600" TEXT="DLP Analytics">
+<node CREATED="1487612401551" FOLDED="true" ID="ID_478925296" MODIFIED="1490630139504" POSITION="left" TEXT="my background/experience">
+<icon BUILTIN="full-1"/>
+<node CREATED="1488311287689" ID="ID_1361141695" MODIFIED="1488311296162" TEXT="DLP implementation and ops">
+<node CREATED="1488311296164" ID="ID_424175078" MODIFIED="1488311305379" TEXT="FS-NPI">
+<node CREATED="1488311319062" ID="ID_1651914048" MODIFIED="1488311322219" TEXT="Divestiture"/>
+<node CREATED="1488313492051" ID="ID_850147318" MODIFIED="1488313502193" TEXT="privacy breach assessments"/>
+<node CREATED="1488313503777" ID="ID_776089242" MODIFIED="1488313510356" TEXT="technology assessments"/>
+</node>
+<node CREATED="1488311306878" ID="ID_884894385" MODIFIED="1488311311683" TEXT="IP-Pharma">
+<node CREATED="1488311312455" ID="ID_1979208938" MODIFIED="1488311316027" TEXT="Divestiture"/>
+</node>
+</node>
+<node CREATED="1488311333415" ID="ID_1773359372" MODIFIED="1488311340195" TEXT="analytics in college">
+<node CREATED="1488311348930" ID="ID_540010878" MODIFIED="1488311354594" TEXT="forecasting/econometrics"/>
+</node>
+</node>
+<node CREATED="1487612806101" ID="ID_1440683848" MODIFIED="1487612902551" POSITION="left" TEXT="using actionable intelligence to reduce risk">
+<icon BUILTIN="idea"/>
+<node CREATED="1487614571955" ID="ID_1262064239" MODIFIED="1487614605872" TEXT="shrinking Security budgets">
+<node CREATED="1487614588419" ID="ID_526145496" MODIFIED="1487614591775" TEXT="smaller staff"/>
+<node CREATED="1487614595476" ID="ID_1055103834" MODIFIED="1487614612968" TEXT="need for increased automation"/>
+<node CREATED="1487612886101" ID="ID_907678437" MODIFIED="1487612897286" TEXT="data centric not user centric (not UBA)">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1487612902552" ID="ID_231453275" MODIFIED="1487612917641" TEXT="correlation across many data sources">
+<node CREATED="1487612941317" FOLDED="true" ID="ID_291502224" MODIFIED="1490631802106" TEXT="establish data residency to a specific &#xa;data asset once it leaves authoritative record">
+<node CREATED="1488311224844" ID="ID_796516188" MODIFIED="1488311238690" TEXT="eg DB/App or ECM"/>
+</node>
+<node CREATED="1487614461076" ID="ID_528074784" MODIFIED="1487614468751" TEXT="provide an organizational view">
+<node CREATED="1487614475484" ID="ID_1140948105" MODIFIED="1487614509984" TEXT="contribution to data sprawl"/>
+<node CREATED="1487614510908" ID="ID_1985832401" MODIFIED="1487614520289" TEXT="compliance with controls"/>
+<node CREATED="1487614553148" ID="ID_920076975" MODIFIED="1488311216418">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      enable a before/after<br />snapshot on #of hits after policy changes
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1487614889899" ID="ID_1927907413" MODIFIED="1487614894815" TEXT="enable blocking/warn messages"/>
+<node CREATED="1487614896098" ID="ID_467945354" MODIFIED="1487614899768" TEXT="policy updates"/>
+</node>
+</node>
+<node CREATED="1487615099371" ID="ID_519273132" MODIFIED="1487615103966" TEXT="trending">
+<node CREATED="1488313451596" ID="ID_1681071899" MODIFIED="1488313465299" TEXT="understand seasonal trends on data creation/access"/>
+</node>
+</node>
+</node>
+<node CREATED="1488313367028" FOLDED="true" ID="ID_1107579236" MODIFIED="1490631581930" POSITION="right" TEXT="what this talk is/isnt">
+<node CREATED="1488313375320" FOLDED="true" ID="ID_384095417" MODIFIED="1490630148219" TEXT="isn&apos;t">
+<node CREATED="1488313378974" ID="ID_1257966036" MODIFIED="1488313391735" TEXT="a position that dlp is completely useless"/>
+<node CREATED="1488313393122" ID="ID_814932607" MODIFIED="1488313407123" TEXT="taking a position on one vendor vs another"/>
+</node>
+<node CREATED="1488313410543" FOLDED="true" ID="ID_1543830378" MODIFIED="1490631579208" TEXT="what this talk is">
+<node CREATED="1487612385432" FOLDED="true" ID="ID_43426094" MODIFIED="1490631578009" TEXT="challenges">
+<icon BUILTIN="full-3"/>
+<node CREATED="1487612417197" ID="ID_1904384483" MODIFIED="1487612424531" TEXT="technical">
+<node CREATED="1487612437974" ID="ID_160673973" MODIFIED="1487612456323" TEXT="many components"/>
+<node CREATED="1487612457478" ID="ID_1950182565" MODIFIED="1487612465971" TEXT="endpoint">
+<node CREATED="1487612467214" ID="ID_1819566935" MODIFIED="1487612472170" TEXT="agent performance"/>
+<node CREATED="1487612472815" ID="ID_1074460409" MODIFIED="1487612482572" TEXT="maintaining updates"/>
+</node>
+<node CREATED="1487612487271" ID="ID_1231590233" MODIFIED="1487612489116" TEXT="mobile"/>
+</node>
+<node CREATED="1487612427391" ID="ID_1913892050" MODIFIED="1487612434331" TEXT="organizational">
+<node CREATED="1487612531205" ID="ID_1227852071" MODIFIED="1487612535203" TEXT="data sprawl"/>
+<node CREATED="1487612538710" ID="ID_589348151" MODIFIED="1487612551227" TEXT="fragmented approach to data protection">
+<node CREATED="1487612985527" ID="ID_936529812" MODIFIED="1487613011755" TEXT="DLP separated from AUP/Privacy functions"/>
+<node CREATED="1487613013469" ID="ID_979291199" MODIFIED="1487613028346" TEXT="not incorporated into enterprise incident response"/>
+<node CREATED="1487613030133" ID="ID_176239083" MODIFIED="1487613994297" TEXT="Classification? what&apos;s that?"/>
+</node>
+</node>
+<node CREATED="1487612923301" ID="ID_1823587741" MODIFIED="1487612926282" TEXT="regulatory">
+<node CREATED="1487612927093" ID="ID_1365265021" MODIFIED="1487612930482" TEXT="GDRP">
+<node CREATED="1487614427516" ID="ID_1718559673" MODIFIED="1487614436576" TEXT="a lot of uncertainty on requirements/impact"/>
+<node CREATED="1487614442421" ID="ID_333090100" MODIFIED="1487614448456" TEXT="data flow mapping"/>
+</node>
+<node CREATED="1487612932502" ID="ID_1826504538" MODIFIED="1488311178503" TEXT="PCI (and everything else) checkbox"/>
+</node>
+</node>
+<node CREATED="1487612516647" FOLDED="true" ID="ID_383746269" MODIFIED="1490631575825" TEXT="problem statement">
+<icon BUILTIN="full-2"/>
+<node CREATED="1487612746823" ID="ID_1756111559" MODIFIED="1487612749595" TEXT="insider threat"/>
+<node CREATED="1487612751383" ID="ID_188333523" MODIFIED="1487612766195" TEXT="users admitting to taking data from prior employers">
+<node CREATED="1488311269650" ID="ID_497760330" MODIFIED="1488311273194" TEXT="citation needed"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1490630756548" ID="ID_898795255" MODIFIED="1490719127935" POSITION="right" TEXT="references">
+<node CREATED="1490630760456" ID="ID_879115300" MODIFIED="1490630761993" TEXT="https://www.forcepoint.com/newsroom/2016/forcepoint-and-ponemon-institute-survey-finds-organizations-challenged-when-monitoring"/>
+<node CREATED="1490630780107" ID="ID_1424031731" MODIFIED="1490630782804" TEXT="https://www.usnews.com/news/best-states/washington/articles/2017-03-01/boeing-investigates-security-breach-after-spreadsheet-shared"/>
+<node CREATED="1490631294510" ID="ID_19874646" MODIFIED="1490631296004" TEXT="http://www.zdnet.com/article/leaked-us-military-files-exposed/"/>
+<node CREATED="1490719127936" ID="ID_183890735" MODIFIED="1490719130004" TEXT="http://www.thehour.com/news/article/Bridgewater-consultant-charged-with-stealing-11028883.php"/>
+</node>
+</node>
+</map>
